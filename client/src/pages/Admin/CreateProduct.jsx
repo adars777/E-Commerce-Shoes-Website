c52d60911b0177344033d81834cf73d880734115
Adarsh Kumar Nishad <69967272+adars777@users.noreply.github.com>
2024-07-28 15:29:21 +0530

    Update CreateProduct.jsx

--- client/src/pages/Admin/CreateProduct.jsx
+++ client/src/pages/Admin/CreateProduct.jsx
@@ -25,7 +25,7 @@ const CreateProduct = () => {
   const getAllCategory = async () => {
     try {
       const { data } = await axios.get(
-        `http://localhost:8080/api/v1/category/get-category`
+        `https://e-commerce-shoes-website-backend.onrender.com/api/v1/category/get-category`
       );
       if (data?.success) {
         setCategories(data?.category);
@@ -52,7 +52,7 @@ const CreateProduct = () => {
       productData.append("photo", photo);
       productData.append("category", category);
       const { data } = await axios.post(
-        `http://localhost:8080/api/v1/product/create-product`,
+        `https://e-commerce-shoes-website-backend.onrender.com/api/v1/product/create-product`,
         productData,
         {
           headers: {
