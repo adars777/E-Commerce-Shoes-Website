e11ab96e727d8398236de55ffac2057bdc2db971
Adarsh Kumar Nishad <69967272+adars777@users.noreply.github.com>
2024-07-28 15:32:31 +0530

    Update Orders.jsx

--- client/src/pages/user/Orders.jsx
+++ client/src/pages/user/Orders.jsx
@@ -12,7 +12,7 @@ const Orders = () => {
     try {
       console.log("hi get order");
       const { data } = await axios.get(
-        `http://localhost:8080/api/v1/auth/orders`
+        `https://e-commerce-shoes-website-backend.onrender.com/api/v1/auth/orders`
       );
       console.log("all data");
       setOrders(data);
