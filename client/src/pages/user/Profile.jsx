c8725649ca8ce45ea64904b9b2c307cb1b60e77f
Adarsh Kumar Nishad <69967272+adars777@users.noreply.github.com>
2024-07-28 15:32:56 +0530

    Update Profile.jsx

--- client/src/pages/user/Profile.jsx
+++ client/src/pages/user/Profile.jsx
@@ -39,7 +39,7 @@ function Profile() {
     e.preventDefault();
     try {
       const { data } = await axios.put(
-        `http://localhost:8080/api/v1/auth/profile`,
+        `https://e-commerce-shoes-website-backend.onrender.com/api/v1/auth/profile`,
         formData,
         {
           headers: {
